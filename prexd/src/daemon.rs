499f2112fef75e9eed7bccdf35eed2a18506aeb8
aceinetx <mouselin82@gmail.com>
2025-07-20 21:48:49 +0000

    Implement info command

--- prexd/src/daemon.rs
+++ prexd/src/daemon.rs
@@ -1,4 +1,4 @@
-use serde_json::Value;
+use serde_json::{Map, Value};
 use std::io::{Read, Write};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::process::Command;
@@ -16,6 +16,60 @@ impl Daemon {
             listener: UnixListener::bind(prex_core::SOCKET_PATH).unwrap(),
             listen: true,
         };
+    }
+
+    fn process_exec_packet(
+        &mut self,
+        map: &serde_json::Map<String, Value>,
+        stream: &mut UnixStream,
+    ) {
+        if let Some(Value::Array(args)) = map.get("args") {
+            let exe_name: String;
+            let mut argv: Vec<String> = args
+                .iter()
+                .filter_map(|arg| arg.as_str().map(String::from))
+                .collect();
+
+            if argv.len() == 0 {
+                eprintln!("Argv is empty!");
+            } else {
+                exe_name = argv.get(0).unwrap().to_string();
+                argv.drain(0..1);
+
+                // Execute the process
+                let child_res = Command::new(exe_name).args(&argv).spawn();
+                if child_res.is_err() {
+                    eprintln!("Failed to spawn a child: {}", child_res.err().unwrap());
+                    // Send back zero indicating an error
+                    let _ = stream.write_all(&(0 as i32).to_ne_bytes());
+                } else {
+                    // Send back the child's PID
+                    let mut child = child_res.ok().unwrap();
+                    let _ = stream.write_all(&child.id().to_ne_bytes());
+
+                    let child_id = child.id();
+                    thread::spawn(move || {
+                        let _ = child.wait();
+                        println!("Child process {} has exited", child_id);
+                    });
+                }
+            }
+        }
+    }
+
+    fn process_info_packet(&mut self, stream: &mut UnixStream) {
+        let mut info = Map::new();
+        info.insert(
+            "pid".to_string(),
+            serde_json::to_value(std::process::id()).unwrap(),
+        );
+        let info_s = serde_json::to_string(&Value::Object(info)).unwrap();
+
+        stream
+            .write_all(&(info_s.len() as i32).to_ne_bytes())
+            .unwrap();
+
+        stream.write_all(info_s.as_bytes()).unwrap();
     }
 
     fn process_packet(&mut self, stream: &mut UnixStream) {
@@ -51,42 +105,10 @@ impl Daemon {
                     if packet_type == prex_core::PACKET_SHUTDOWN {
                         println!("Daemon is shutting down...");
                         self.listen = false;
+                    } else if packet_type == prex_core::PACKET_INFO {
+                        self.process_info_packet(stream);
                     } else if packet_type == prex_core::PACKET_EXEC {
-                        if let Some(Value::Array(args)) = map.get("args") {
-                            let exe_name: String;
-                            let mut argv: Vec<String> = args
-                                .iter()
-                                .filter_map(|arg| arg.as_str().map(String::from))
-                                .collect();
-
-                            if argv.len() == 0 {
-                                eprintln!("Argv is empty!");
-                            } else {
-                                exe_name = argv.get(0).unwrap().to_string();
-                                argv.drain(0..1);
-
-                                // Execute the process
-                                let child_res = Command::new(exe_name).args(&argv).spawn();
-                                if child_res.is_err() {
-                                    eprintln!(
-                                        "Failed to spawn a child: {}",
-                                        child_res.err().unwrap()
-                                    );
-                                    // Send back zero indicating an error
-                                    let _ = stream.write_all(&(0 as i32).to_ne_bytes());
-                                } else {
-                                    // Send back the child's PID
-                                    let mut child = child_res.ok().unwrap();
-                                    let _ = stream.write_all(&child.id().to_ne_bytes());
-
-                                    let child_id = child.id();
-                                    thread::spawn(move || {
-                                        let _ = child.wait();
-                                        println!("Child process {} has exited", child_id);
-                                    });
-                                }
-                            }
-                        }
+                        self.process_exec_packet(&map, stream);
                     }
                 }
             } else {
